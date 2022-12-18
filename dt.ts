48fc882cc41dbdfcf7c7cc8efad9260c345d13a2
Francis Targanski <francis@targanski.com>
2022-12-18 17:06:21 -0300

    config adminjs connection

--- dt.ts
+++ dt.ts
@@ -1,7 +1,20 @@
-import { Sequelize } from 'sequelize'
+import { Sequelize } from 'sequelize';
+import mongoose from 'mongoose';
+require('dotenv').config()
 
-const sequelize = new Sequelize('postgres://adminjs:adminjs@localhost:5435/adminjs', {
-  dialect: 'mysql',
+const { SQL_DB_USER, SQL_DB_PASS, SQL_DB_HOST, SQL_DB_PORT, SQL_DB_NAME,
+        NOSQL_DB_USER, NOSQL_DB_PASS, NOSQL_DB_HOST, NOSQL_DB_PORT, NOSQL_DB_NAME } = process.env;
+
+mongoose.connect(`mongodb+srv://${NOSQL_DB_USER}:${NOSQL_DB_PASS}@${NOSQL_DB_HOST}:${NOSQL_DB_PORT}/${NOSQL_DB_NAME}?retryWrites=true&w=majority`)
+
+// /adminjs-mongo`
+
+const mongooseDB = mongoose.Connection;
+const sequelizeDb = new Sequelize(`mysql://${SQL_DB_USER}:${SQL_DB_PASS}@${SQL_DB_HOST}:${SQL_DB_PORT}/${SQL_DB_NAME}`,{
+    dialect: 'mysql'
 })
 
-export default sequelize
+export {
+    sequelizeDb,
+    mongoose
+}
